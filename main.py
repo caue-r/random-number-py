6155bc0ceb5a8b4501fa1c4ebdf443e29cae55de
Cauê Reis <cauereis846@gmail.com>
2026-01-09 15:47:56 -0300

    feat: Criando funções e adicionando opção do usuário escolher max range do número aleatório

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import time 
 import hashlib
 
-b = time.perf_counter_ns()
 
-ruido = []
+def jitter_random_256bits(iteracoes):
+    b = time.perf_counter_ns()
 
-for i in range(10000):
-    a1 = time.perf_counter_ns()
-    a2 = time.perf_counter_ns()
-    c1 = time.perf_counter_ns()
-    c2 = time.perf_counter_ns()
+    ruido = []
 
-    deltaA = a2 - a1
-    deltaC = c2 - c1
+    for i in range(10000):
+        a1 = time.perf_counter_ns()
+        a2 = time.perf_counter_ns()
+        c1 = time.perf_counter_ns()
+        c2 = time.perf_counter_ns()
 
-    # PCG 64-bit - 6364136223846793005 
-    b = (b * 6364136223846793005 + deltaA + deltaC) & ((1 << 64) - 1)
-    ruido.append(b)
+        deltaA = a2 - a1
+        deltaC = c2 - c1
 
-numeroRandom = hashlib.sha256(str(ruido).encode()).hexdigest()
+        # PCG 64-bit - 6364136223846793005 
+        b = (b * 6364136223846793005 + deltaA + deltaC) & ((1 << 64) - 1)
+        ruido.append(b)
+    hash_hex = hashlib.sha256(str(ruido).encode()).hexdigest()
+    return int(hash_hex, 16)
 
-numeroRandom = int(numeroRandom, 16)
+def random_input_user(maxValor):
+    n = jitter_random_256bits(2000) #default 2000 iteracoes
+    return n % (maxValor + 1)
 
-print(numeroRandom)
+max_valor = int(input("Max range: "))
+numero= random_input_user(max_valor)
+print(numero)
